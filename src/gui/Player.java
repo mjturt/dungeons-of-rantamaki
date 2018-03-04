3afb0c76b27ddb7fa0d10ab1a66aa415a4750cfe
vsahav <vsahav@utu.fi>
2018-03-04 22:46:46 +0200

    Changes to collision detection
    Upgraded and improved collision detection, no more getting stuck inside
    walls or random bounces outside of play area.

--- src/gui/Player.java
+++ src/gui/Player.java
@@ -9,89 +9,75 @@ import java.awt.image.BufferedImage;
 
 public class Player extends GameObject {
 
-    Handler handler;
-    private BufferedImage playerimg = null;
+	Handler handler;
+	private BufferedImage playerimg = null;
+	int tempX;
+	int tempY;
 
-    public Player(int x, int y, ID id, Handler handler) {
-        super(x, y, id);
-        this.handler = handler;
-        ImageLoader loader = new ImageLoader();
-        playerimg = loader.loadImage("/player.png");
-    }
+	public Player(int x, int y, ID id, Handler handler) {
+		super(x, y, id);
+		this.handler = handler;
+		ImageLoader loader = new ImageLoader();
+		playerimg = loader.loadImage("/player.png");
+	}
 
-    /* Players own ticking and rendering methods */
+	/* Players own ticking and rendering methods */
 
-    public void tick() {
+	public void tick() {
 
-        /* Moving coordinates based on velocity */
+		/* Moving coordinates based on velocity */
 
-        
+		/* Moving, using velocity instead of coordinates for smooth movement */
 
-        /* Moving, using velocity instead of coordinates for smooth movement */
+		if (handler.isUp()) {
+			velY = -2;
+		} else if (!handler.isDown()) {
+			velY = 0;
+		}
+		if (handler.isDown()) {
+			velY = 2;
+		} else if (!handler.isUp()) {
+			velY = 0;
+		}
+		if (handler.isRight()) {
+			velX = 2;
+		} else if (!handler.isLeft()) {
+			velX = 0;
+		}
+		if (handler.isLeft()) {
+			velX = -2;
+		} else if (!handler.isRight()) {
+			velX = 0;
+		}
+		tempX = x;
+		tempX += velX;
+		tempY = y;
+		tempY += velY;
+		
+		updatePos(tempY, tempX);
+	}
 
-        if (handler.isUp()) {
-            if (collides(this.handler)) {
-            	velY = 2;
-            	this.handler.setUp(true);
-            } else {
-        	velY = -2;
-            }
-        } else if (!handler.isDown()) {
-            velY = 0;
-        }
-        if (handler.isDown()) {
-        	if (collides(this.handler)) {
-        		velY = -2;
-        		this.handler.setDown(true);
-        	} else {
-        		velY = 2;
-        	}
-        } else if (!handler.isUp()) {
-            velY = 0;
-        }
-        if (handler.isRight()) {
-            if (collides(this.handler)) {
-            	velX = -2;
-            	this.handler.setRight(true);
-            } else {
-            	velX = 2;
-            }
-        } else if (!handler.isLeft()) {
-            velX = 0;
-        }
-        if (handler.isLeft()) {
-        	if (collides(this.handler)) {
-        		velX = 2;
-        		this.handler.setLeft(true);
-        	} else {
-        		velX = -2;
-        	}
-        } else if (!handler.isRight()) {
-            velX = 0;
-        }
-        
-        x += velX;
-        y += velY;
-    }
 
-    public void render(Graphics g) {
-        //g.setColor(Color.green);
-        //g.fillRect(x, y, 32, 32);
-        /* Current image is just example and its too big */
-        g.drawImage(playerimg, x, y, null);
+	public void render(Graphics g) {
+		// g.setColor(Color.green);
+		// g.fillRect(x, y, 32, 32);
+		/* Current image is just example and its too big */
+		g.drawImage(playerimg, x, y, null);
 
-    }
+	}
 
-    public Rectangle getBounds() {
-        return new Rectangle(x, y, 32, 32);
-    }
-    
-    public boolean collides(Handler h) {
-    	for (GameObject go : h.objects) {
-    		if (go.getBounds().intersects(this.getBounds()) && go.getClass() == Block.class) {
-    			return true;
-    		}
-    	}
-    	return false;
-    }
+	public Rectangle getBounds() {
+		return new Rectangle(x, y, 32, 32);
+	}
+
+	public void updatePos(int newY, int newX) {
+		Rectangle newPos = new Rectangle(newX, newY, 32, 32);
+		for (GameObject o : handler.objects) {
+			if (newPos.intersects(o.getBounds()) && o.getClass() == Block.class) {
+				return;
+			}
+		}
+		y = tempY;
+		x = tempX;
+	}
 }
