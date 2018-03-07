2fcb547009f286237ab52a510c5b599ccb4b3242
vsahav <vsahav@utu.fi>
2018-03-08 01:45:06 +0200

    Combat system ready for action
    Combat system and looting works now. Need UI tweaking and more displayed
    information.

--- src/gui/InitCombatProper.java
+++ src/gui/InitCombatProper.java
@@ -17,6 +17,8 @@ public class InitCombatProper extends JDialog implements ActionListener {
 	private JFrame jf;
 	private JTextField playerText = new JTextField();
 	private JTextField enemyText = new JTextField();
+	private ArrayList<Consumable> loot;
+	private boolean looted;
 
 	public InitCombatProper(Player p, Monster m, JFrame jf) {
 		this.running = true;
@@ -26,6 +28,8 @@ public class InitCombatProper extends JDialog implements ActionListener {
 		this.m = m;
 		this.p = p;
 		this.jd.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		this.loot = generateLoot();
+		this.looted = false;
 	}
 
 	public void createMain() {
@@ -62,12 +66,12 @@ public class InitCombatProper extends JDialog implements ActionListener {
 		this.jd.repaint();
 	}
 
-	private void createConsumables() {
+	private void createConsumablesMenu() {
 		System.out.println("started creating consumables");
 		this.jd.getContentPane().removeAll();
 		this.jd.setVisible(true);
 		JPanel selectConsumable = new JPanel();
-		ArrayList<JButton> buttons = createButtons(getConsumableNames());
+		ArrayList<JButton> buttons = createButtons(getInventoryItemNames());
 		for (int i = 0; i < buttons.size(); i++) {
 			System.out.println("creating...");
 			selectConsumable.add(buttons.get(i));
@@ -87,7 +91,7 @@ public class InitCombatProper extends JDialog implements ActionListener {
 		this.jd.setVisible(true);
 	}
 
-	private void createAttacks() {
+	private void createAttacksMenu() {
 		this.jd.getContentPane().removeAll();
 		this.jd.setVisible(true);
 		JPanel selectAttackType = new JPanel();
@@ -114,7 +118,7 @@ public class InitCombatProper extends JDialog implements ActionListener {
 		this.jd.setVisible(true);
 	}
 
-	private void createPhysicals() {
+	private void createPhysicalsMenu() {
 		System.out.println("started creating physical attacks");
 		this.jd.getContentPane().removeAll();
 		this.jd.setVisible(true);
@@ -139,7 +143,7 @@ public class InitCombatProper extends JDialog implements ActionListener {
 		this.jd.setVisible(true);
 	}
 
-	private void createMagicals() {
+	private void createMagicalsMenu() {
 		System.out.println("started creating magical attacks!");
 		this.jd.getContentPane().removeAll();
 		this.jd.setVisible(true);
@@ -188,13 +192,84 @@ public class InitCombatProper extends JDialog implements ActionListener {
 		buttons.add(ret);
 		return buttons;
 	}
-	
-	private void isAlive() {
-		if (this.p.getHP() < 1 || this.m.getHP() < 1) {
-			this.running = false;
-			this.jf.setEnabled(true);
-			jd.dispatchEvent(new WindowEvent(jd, WindowEvent.WINDOW_CLOSING));
+
+	private ArrayList<JButton> createButtonsLoot(ArrayList<String> names) {
+		ArrayList<JButton> buttons = new ArrayList<>();
+		int n = 0;
+		for (int i = 0; i < names.size(); i++) {
+			JButton b = new JButton(names.get(i));
+			b.setName(names.get(i));
+			b.addActionListener(new Loot(this));
+			b.setActionCommand(names.get(i));
+			b.setSize(64, 32);
+			b.setMnemonic(i);
+			System.out.println(b.getActionCommand());
+			buttons.add(b);
+			n = i;
 		}
+		JButton ret = new JButton("DISCARD ALL");
+		ret.setName("DISCARD ALL");
+		ret.addActionListener(new Loot(this));
+		ret.setActionCommand("DISCARD ALL");
+		ret.setSize(64, 32);
+		ret.setMaximumSize(new Dimension(64, 32));
+		ret.setMnemonic(n);
+		buttons.add(ret);
+		return buttons;
+	}
+
+	public void stillAlive() {
+		if (this.p.getHP() > 0 && this.m.getHP() > 0) {
+			monsterRandomAttack();
+		} else if (this.p.getHP() > 0 && this.m.getHP() <= 0) {
+			createLootMenu();
+		} else {
+			kill();
+		}
+	}
+	
+	public void kill() {
+		this.running = false;
+		this.jf.setEnabled(true);
+		jd.dispatchEvent(new WindowEvent(jd, WindowEvent.WINDOW_CLOSING));
+	}
+
+	public void createLootMenu() {
+		System.out.println("started creating loot");
+		this.jd.getContentPane().removeAll();
+		this.jd.setVisible(true);
+		JPanel selectConsumable = new JPanel();
+		ArrayList<JButton> buttons = createButtonsLoot(getConsumableNames(this.loot));
+		for (int i = 0; i < buttons.size(); i++) {
+			System.out.println("creating...");
+			selectConsumable.add(buttons.get(i));
+			System.out.println("Added: " + buttons.get(i).getName());
+		}
+		System.out.println("finished adding buttons");
+		this.playerText.setSize(64, 64);
+		this.enemyText.setSize(64, 64);
+		this.jd.getContentPane().add(selectConsumable);
+		this.jd.setLayout(new FlowLayout());
+		this.jd.revalidate();
+		this.jd.repaint();
+		this.jd.setVisible(true);
+	}
+
+	public ArrayList<Consumable> generateLoot() {
+		ArrayList<Consumable> loot = new ArrayList<>();
+		ItemGenerator lst = new ItemGenerator();
+		Random r = new Random();
+		int maxLoot = m.getLevel() / 10;
+		if (maxLoot < 1) {
+			maxLoot = r.nextInt(2);
+			System.out.println("MaxLoot: " + maxLoot);
+		}
+		if (maxLoot > 0) {
+			for (int i = 0; i < maxLoot; i++) {
+				loot.add(lst.getItem(r.nextInt(lst.getListSize())));
+			}
+		}
+		return loot;
 	}
 
 	private ArrayList<String> getSpellNames() {
@@ -205,6 +280,14 @@ public class InitCombatProper extends JDialog implements ActionListener {
 		return spellnames;
 	}
 
+	private ArrayList<String> getConsumableNames(ArrayList<Consumable> list) {
+		ArrayList<String> itemNames = new ArrayList<>();
+		for (Consumable c : this.loot) {
+			itemNames.add(c.getConsumableName());
+		}
+		return itemNames;
+	}
+
 	private ArrayList<String> getAttackNames() {
 		ArrayList<String> attacks = new ArrayList<>();
 		for (combat.Attack a : this.p.getMovelist()) {
@@ -213,15 +296,15 @@ public class InitCombatProper extends JDialog implements ActionListener {
 		return attacks;
 	}
 
-	private ArrayList<String> getConsumableNames() {
+	private ArrayList<String> getInventoryItemNames() {
 		ArrayList<String> consumables = new ArrayList<>();
 		for (Consumable c : this.p.getInventory()) {
 			consumables.add(c.getConsumableName());
 		}
 		return consumables;
 	}
-	
-	private void monsterRandomAttack () {
+
+	private void monsterRandomAttack() {
 		Random r = new Random();
 		this.m.DealDamage(this.p, this.m.getMovelist().get(r.nextInt(this.m.getMovelist().size())));
 	}
@@ -261,53 +344,118 @@ public class InitCombatProper extends JDialog implements ActionListener {
 		this.running = running;
 	}
 
+	public JFrame getJf() {
+		return jf;
+	}
+
+	public void setJf(JFrame jf) {
+		this.jf = jf;
+	}
+
+	public JTextField getPlayerText() {
+		return playerText;
+	}
+
+	public void setPlayerText(JTextField playerText) {
+		this.playerText = playerText;
+	}
+
+	public JTextField getEnemyText() {
+		return enemyText;
+	}
+
+	public void setEnemyText(JTextField enemyText) {
+		this.enemyText = enemyText;
+	}
+
+	public ArrayList<Consumable> getLoot() {
+		return loot;
+	}
+
+	public void setLoot(ArrayList<Consumable> loot) {
+		this.loot = loot;
+	}
+
 	/*
 	 * GETTERS AND SETTERS END
 	 */
+
+	public boolean isLooted() {
+		return looted;
+	}
+
+	public void setLooted(boolean looted) {
+		this.looted = looted;
+	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		System.out.println("ActionCommand: " + e.getActionCommand());
 		if ("CONSUMABLES".equals(e.getActionCommand())) {
-			createConsumables();
+			createConsumablesMenu();
 		} else if ("ATTACK".equals(e.getActionCommand())) {
-			createAttacks();
+			createAttacksMenu();
 		} else if ("RETURN".equals(e.getActionCommand())) {
 			jd.getContentPane().removeAll();
 			createMain();
 		} else if ("MAGICAL".equals(e.getActionCommand())) {
-			createMagicals();
+			createMagicalsMenu();
 		} else if ("PHYSICAL".equals(e.getActionCommand())) {
-			createPhysicals();
+			createPhysicalsMenu();
 		} else {
 			for (int i = 0; i < this.p.getInventory().size(); i++) {
 				if (e.getActionCommand().equals(this.p.getInventory().get(i).getConsumableName())) {
 					this.p.useItem(i);
-					monsterRandomAttack();
 					jd.getContentPane().removeAll();
 					createMain();
-					isAlive();
+					stillAlive();
 				}
 			}
 			for (int i = 0; i < this.p.getSpellbook().size(); i++) {
 				Attack a = this.p.getSpellbook().get(i);
 				if (e.getActionCommand().equals(a.getName())) {
 					this.p.DealDamage(this.m, a);
-					monsterRandomAttack();
 					jd.getContentPane().removeAll();
 					createMain();
-					isAlive();
+					stillAlive();
 				}
 			}
 			for (int i = 0; i < this.p.getMovelist().size(); i++) {
 				Attack a = this.p.getMovelist().get(i);
 				if (e.getActionCommand().equals(a.getName())) {
 					this.p.DealDamage(this.m, a);
-					monsterRandomAttack();
 					jd.getContentPane().removeAll();
 					createMain();
-					isAlive();
+					stillAlive();
 				}
+			}
+		}
+	}
+}
+
+class Loot implements ActionListener {
+
+	InitCombatProper ic;
+
+	public Loot(InitCombatProper ic) {
+		this.ic = ic;
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if ("DISCARD ALL".equals(e.getActionCommand())) {
+			this.ic.stillAlive();
+		}
+		ArrayList<Consumable> temp = this.ic.getLoot();
+		if (temp.size() == 0) {
+			this.ic.kill();
+		}
+		for (int i = 0; i < temp.size(); i++) {
+			if (e.getActionCommand().equals(temp.get(i).getConsumableName())) {
+				this.ic.getP().addItem(temp.get(i));
+				this.ic.getLoot().remove(temp.get(i));
+				this.ic.getJd().getContentPane().removeAll();
+				this.ic.createLootMenu();
 			}
 		}
 	}
